2f4dca6aa0a8174c92dbdf11a87064282fb6b7f3
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-08 17:33:54 +0300

    Add task1

---
+++ DZ3/src/part1/Main.java
@@ -0,0 +1,20 @@
+package part1;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //File testFile = new File("test.txt");
+        writeFromConsole();
+    }
+    public static void writeFromConsole() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        try (BufferedWriter writer = new BufferedWriter( new FileWriter( "test.txt" ))){
+            //line = reader.readLine();
+            while (!(line = reader.readLine()).isEmpty()){
+                writer.write(line + "\n");
+            }
+        }
+    }
+}
