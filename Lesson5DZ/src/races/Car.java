673fe227ce09e06c8bdc5b33ce430b95847025cb
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-10 13:06:51 +0300

    Cars starts when everyone is ready

--- Lesson5DZ/src/races/Car.java
+++ Lesson5DZ/src/races/Car.java
@@ -1,5 +1,7 @@
 package races;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
@@ -8,17 +10,20 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier cbReady;
+
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cbReady) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cbReady = cbReady;
     }
     @Override
     public void run() {
@@ -26,6 +31,7 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cbReady.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
