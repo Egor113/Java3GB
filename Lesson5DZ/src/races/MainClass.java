144d9bed52cf2e4704549eddf3a494290a25de31
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-10 13:10:36 +0300

    The announce "The race starts" come when everyone is ready

--- Lesson5DZ/src/races/MainClass.java
+++ Lesson5DZ/src/races/MainClass.java
@@ -1,12 +1,13 @@
 package races;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        CyclicBarrier cbReady = new CyclicBarrier(4);
+        CyclicBarrier cbReady = new CyclicBarrier(5);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
@@ -14,6 +15,13 @@ public class MainClass {
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
+        }
+        try {
+            cbReady.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
         }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
