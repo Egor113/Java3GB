e41f18dc418113d6d4c1d8b6f1c2853c4a92fb61
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-11 22:12:32 +0300

    Created ArrayOf1And4

--- Lesson6DZ/Logging/src/main/java/part3/ArrayOf1And4.java
+++ Lesson6DZ/Logging/src/main/java/part3/ArrayOf1And4.java
@@ -1,4 +1,15 @@
 package part3;
 
 public class ArrayOf1And4 {
+
+    public boolean checkArray(int[] arr){
+        boolean flag = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1 || arr[i] == 4){
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
 }
