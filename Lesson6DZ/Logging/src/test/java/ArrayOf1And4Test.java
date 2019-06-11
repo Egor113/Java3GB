3c42dbc2c80dcf62997cf4d8e9a1584fcbd382ea
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-11 22:12:42 +0300

    Created ArrayOf1And4Test

--- Lesson6DZ/Logging/src/test/java/ArrayOf1And4Test.java
+++ Lesson6DZ/Logging/src/test/java/ArrayOf1And4Test.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import part3.ArrayOf1And4;
 
 public class ArrayOf1And4Test {
+    private static ArrayOf1And4 array;
+
+    @BeforeClass
+    public static void init(){
+        System.out.println("init array");
+        array = new ArrayOf1And4();
+    }
+
+    @Test
+    public void test1() {
+        Assert.assertEquals(array.checkArray(new int[]{1,2,3,4,5,6,7}),true);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(array.checkArray(new int[]{1,2,3}),false);
+    }
 }
