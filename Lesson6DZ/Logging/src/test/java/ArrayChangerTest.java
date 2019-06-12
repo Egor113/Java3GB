16933895a660e4a10975e8fa93ea554da78b55f5
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-12 12:44:05 +0300

    Add tests to ArrayChangerParams

--- Lesson6DZ/Logging/src/test/java/ArrayChangerTest.java
+++ Lesson6DZ/Logging/src/test/java/ArrayChangerTest.java
@@ -1,40 +1,42 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import part2.ArrayChanger;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
-@RunWith(value = Parameterized.class)
-public class ArrayChangerTest {
-    private static ArrayChanger changer = null;
-    private int[] res;
+public class ArrayChangerTest2 {
+    private static ArrayChanger changer;
 
-    private int[] arr;
-
-    public ArrayChangerTest(int[] arr) {
-        this.arr = arr;
+    @BeforeClass
+    public static void init(){
+        System.out.println("init changer");
+        changer = new ArrayChanger();
     }
-
-    @Parameters
-    public static int[] abracadabra() {
-        return new int[]{1,2,3,4};
-    }
-
-//    @Parameters
-//    public static Collection abracadabra() {
-//        return Arrays.asList(new Object[][]{{
-//                new int[]{1,2,3,4,5}}}
-//        );
-//    }
 
     @Test
     public void test1(){
-        Assert.assertArrayEquals(changer.changeArray(arr),new int[]{5});
+        //Assert.assertArrayEquals(new int[]{1,2,3,4,5},new int[]{5});
+        Assert.assertArrayEquals(changer.changeArray(new int[]{1,2,3,4,5}),new int[]{5});
     }
 
+    @Test
+    public void test2(){
+        Assert.assertArrayEquals(changer.changeArray(new int[]{1,2,4,98,5,47,6,4,25,0,7}),new int[]{25,0,7});
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test3(){
+        changer.changeArray(new int[]{5,78,44,247,5});
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test4(){
+        changer.changeArray(new int[]{4,4,4,4,4,4,4,4,4});
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void test5(){
+        changer.changeArray(new int[]{15,4,89,6,78,2,4});
+    }
 }
---
+++ Lesson6DZ/Logging/src/test/java/ArrayChangerTestParams.java
@@ -0,0 +1,52 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import part2.ArrayChanger;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class ArrayChangerTest {
+    private static ArrayChanger changer = null;
+    private int[] res;
+
+    private int[] arrNormal;
+    private int[] arrExcept;
+
+    public ArrayChangerTest(int[] arrNormal, int[] arrExcept, int[] res) {
+        this.arrNormal = arrNormal;
+        this.arrExcept = arrExcept;
+        this.res = res;
+    }
+
+    @Before
+    public void init() {
+        System.out.println("init changer");
+        changer = new ArrayChanger();
+    }
+
+
+    @Parameters
+    public static Collection array1() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1,2,3,4,5},new int[]{4,4,4,4,4},new int[]{5}},
+                {new int[]{4,58,44,9,6,4,25,78,9,5},new int[]{1,2,3,4},new int[]{25,78,9,5}},
+                {new int[]{4,25,100,9,2},new int[]{9,25,8,7,2,3,5},new int[]{25,100,9,2}}}
+        );
+    }
+
+    @Test()
+    public void changeArrayNormal(){
+        Assert.assertArrayEquals(changer.changeArray(arrNormal),res);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void changeArrayException(){
+        Assert.assertArrayEquals(changer.changeArray(arrExcept),res);
+    }
+
+}
