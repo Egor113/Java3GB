e25504434be7a930e423ce9a1771f9efefd8331c
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-12 14:55:32 +0300

    Completed part2

--- Lesson1DZ/src/part2/ArrayToList.java
+++ Lesson1DZ/src/part2/ArrayToList.java
@@ -1,4 +1,23 @@
 package part2;
 
-public class ArrayToList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayToList <T>{
+    private List<T> list = null;
+
+    public void convertArrayToList(T[] arr){
+        List<T> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        this.list = list;
+    }
+
+    public void printList(){
+        for (T element: list) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
 }
