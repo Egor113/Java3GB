7af026b6a8deba16769e3220f40fd0a1ebc9dd95
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-12 14:06:57 +0300

    Deleted lesson 6

--- Lesson6DZ/Logging/src/main/java/part3/Main3.java
+++
@@ -1,8 +0,0 @@
-package part3;
-
-public class Main3 {
-    public static void main(String[] args) {
-        ArrayOf1And4 array = new ArrayOf1And4();
-        System.out.println(array.checkArray(new int[]{1,2,3}));
-    }
-}
