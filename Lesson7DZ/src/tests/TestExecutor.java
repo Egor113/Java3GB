65e57f2c3fcc85e3babb1b38ec9c96237668669e
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-16 22:20:25 +0300

    Fixed testing with annotation

--- Lesson7DZ/src/tests/TestExecutor.java
+++ Lesson7DZ/src/tests/TestExecutor.java
@@ -8,23 +8,54 @@ public class TestExecutor {
         try {
             Class someClass = test.getClass();
             CalculatorTest class1 = (CalculatorTest) someClass.newInstance();
-            for (Method m : class1.getClass().getDeclaredMethods()) {
-                if (m.isAnnotationPresent(BeforeSuite.class)) {
-                    m.invoke(class1);
-                }
-                else if (m.isAnnotationPresent(Test.class)) {
-                    System.out.println(m.invoke(class1,1,2,3));
-                }
-                else if (m.isAnnotationPresent(AfterSuite.class)) {
-                    m.invoke(class1);
-                }
-            }
+            runAllMethodsWithBeforeBeforeSuite(class1);
+            runAllMethodsWithTest(class1);
+            runAllMethodsWithBeforeAfterSuite(class1);
+
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void runAllMethodsWithBeforeBeforeSuite(CalculatorTest test) throws InvocationTargetException, IllegalAccessException {
+        Method[] methods = test.getClass().getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].isAnnotationPresent(BeforeSuite.class)){
+                methods[i].invoke(test);
+            }
+        }
+    }
+
+    public static void runAllMethodsWithTest(CalculatorTest test) throws InvocationTargetException, IllegalAccessException {
+        Method[] methods = test.getClass().getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].isAnnotationPresent(Test.class)){
+                if(methods[i].getAnnotation(Test.class).priority() == 1){
+                    System.out.println(methods[i].invoke(test,3,8,11));
+                }
+                else if(methods[i].getAnnotation(Test.class).priority() == 2){
+                    System.out.println(methods[i].invoke(test,10,5,5));
+                }
+                else if(methods[i].getAnnotation(Test.class).priority() == 3){
+                    System.out.println(methods[i].invoke(test,100,2,200));
+                }
+                else if(methods[i].getAnnotation(Test.class).priority() == 4){
+                    System.out.println(methods[i].invoke(test,30,10,3));
+                }
+            }
+        }
+    }
+
+    public static void runAllMethodsWithBeforeAfterSuite(CalculatorTest test) throws InvocationTargetException, IllegalAccessException {
+        Method[] methods = test.getClass().getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].isAnnotationPresent(AfterSuite.class)){
+                methods[i].invoke(test);
+            }
         }
     }
 
