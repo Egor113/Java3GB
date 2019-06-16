f11fb4862561054972855cbb653aee919b1c66ac
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-06-16 15:13:17 +0300

    Renamed class Test to TestAnnotation

---
+++ Lesson7DZ/src/tests/TestAnnotation.java
@@ -0,0 +1,20 @@
+package tests;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+public @interface Test
+{
+    boolean test();
+
+    boolean beforeSuite();
+
+    boolean 
+
+
+}
